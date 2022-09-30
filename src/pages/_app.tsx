6e25a381835fdfd6dc3b7f9ff7bc7ce496e98fa8
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-09-30 10:30:38 -0300

    fix viewport on mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width"/>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
