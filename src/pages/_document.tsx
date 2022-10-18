a8cda2dc8e7291ff62ea981046f10b503a581ba5
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-10-17 23:13:44 -0300

    optimize

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import DeferNextScript from '../helpers/NextScript'
 
 export default function Document() {
   return (
@@ -10,7 +11,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-        <NextScript />
+        <DeferNextScript />
       </body>
     </Html>
   )
